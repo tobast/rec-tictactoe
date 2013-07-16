bb7dab5740d364bc17d1811b08683d6bf19b3b23
tobast <tobast@tobast.fr>
2013-07-16 14:08:06 +0200

    began main.cpp, added a to-implement translation function

---
+++ tools.h
@@ -0,0 +1,44 @@
+
+/*
+ * SOFTWARE:
+ *   Recursive Tic-Tac-Toe
+ * 
+ * AUTHOR:
+ *   Theophile BASTIAN "Tobast" <contact@tobast.fr>
+ *
+ * WEBSITE:
+ *   http://tobast.fr/
+ *
+ * LICENCE:
+ *   GNU GPL v3
+ *
+ * DESCRIPTION:
+ *   This software implements a recursive version of the Tic-Tac-Toe.
+ *
+ * LICENCE HEADER:
+ *   Copyright (C) 2013  BASTIAN Theophile
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/gpl.txt>.
+ */
+
+#ifndef DEF_TOOLS
+#define DEF_TOOLS
+
+#include <string>
+
+/** Translates a string into another language (TODO) **/
+std::string tl(const std::string& str);
+
+#endif//DEF_TOOLS
+
